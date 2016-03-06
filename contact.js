6a096299815013d23d8dca754a4e5ce0201d2e19
Ben Toll <tollben@umich.edu>
2016-03-06 14:06:48 -0500

    Got the map centered. Finally.

--- contact.js
+++ contact.js
@@ -1,14 +1,10 @@
-  
-
-
-  function initialize() {
-    var mapCanvas = document.getElementById('map');
-    var mapOptions = {
-      center: new google.maps.LatLng(44.5403, -78.5463),
-      zoom: 8,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-  }
-
-  google.maps.event.addDomListener(window, 'load', initialize);
+function initialize() {
+	var map_canvas = document.getElementById("google_map");
+	var map_properties = {
+		center:new google.maps.LatLng(42.292989,-83.7106572),
+		zoom:15,
+		mapTypeId:google.maps.MapTypeId.ROADMAP		
+	};
+	var map=new google.maps.Map(map_canvas, map_properties);
+}
+google.maps.event.addDomListener(window, 'load', initialize);	
